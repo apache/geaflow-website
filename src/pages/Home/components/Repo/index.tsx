150298e4c45ae5702f9617a137e6b71edf0d3b3a
liukaiming-alipay <lkm01250564@antgroup.com>
2025-10-11 14:06:44 +0800

    fix: update repo card icon

--- src/pages/Home/components/Repo/index.tsx
+++ src/pages/Home/components/Repo/index.tsx
@@ -36,7 +36,7 @@ const Repo = () => {
             {
                 repos.map((item) => <div key={item.repo} className={styles.repoCard}>
                     <div className={styles.repoCardTitle}>
-                        <img src='https://mdn.alipayobjects.com/huamei_tu4rvn/afts/img/A*IZUlSI-VOkUAAAAAJSAAAAgAep_eAQ/original' alt="logo" />
+                        <img src={'/img/card-logo.svg'} alt="logo" />
                         <a href={`https://github.com/TuGraph-family/${item.repo}`} target='_blank'  >{item.repo}</a>
                         <div className={styles.repoCardTitleTag}>{item.visibility?.replace(/[a-z]/, (val: string) => val.toLocaleUpperCase())}</div>
                     </div>
@@ -45,8 +45,8 @@ const Repo = () => {
                     </div>
                     <div className={styles.repoCardInfo}>
                         {item?.language ? <div><div className={styles.circle} style={{ backgroundColor: LANGUAGE_ICON_COLOR?.[item.language] || '#dea584' }} />{item.language}</div> : null}
-                        {item?.stargazers_count ? <div className={styles.herf} onClick={() => onLink(item.repo, 'stargazers')}><img src="https://mdn.alipayobjects.com/huamei_tu4rvn/afts/img/A*X0eERqyLMw0AAAAAKrAAAAgAep_eAQ/original" alt="" /> {item.stargazers_count}</div> : null}
-                        {item?.forks_count ? <div className={styles.herf} onClick={() => onLink(item.repo, 'forks')}><img src="https://mdn.alipayobjects.com/huamei_tu4rvn/afts/img/A*D4RdTpfrhLMAAAAAI-AAAAgAep_eAQ/original" alt="" /> {item.forks_count}</div> : null}
+                        {item?.stargazers_count ? <div className={styles.herf} onClick={() => onLink(item.repo, 'stargazers')}><img src={'/img/collect.svg'} alt="" /> {item.stargazers_count}</div> : null}
+                        {item?.forks_count ? <div className={styles.herf} onClick={() => onLink(item.repo, 'forks')}><img src={'/img/fork.svg'} alt="" /> {item.forks_count}</div> : null}
                     </div>
                 </div>)
             }
